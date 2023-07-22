2b12e4e1ee20cc2562d8d495e6f0c4a2d35d9565
Ashish Kumar <ashish@192.168.0.100>
2023-07-22 19:19:11 +0530

    commit from kala

---
+++ app.js
@@ -0,0 +1,21 @@
+// Require
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+const mongoose = require("mongoose");
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get("/" , function(req , res)
+{
+    res.render("home");
+})
+
+app.listen(3000, function() 
+{
+    console.log("Server started on port 3000");
+});
