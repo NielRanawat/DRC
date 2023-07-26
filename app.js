9b45e73eb1bb9ce5687d3cad5b7b4267fff5f870
Ashish Kumar <ashish@192.168.1.4>
2023-07-26 20:38:06 +0530

    added carousel n card link

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 const path = require('path')
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get("/" , function(req , res)
 {
