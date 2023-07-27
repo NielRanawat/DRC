248ea2035bb80db0e8a35cc832112889bf14df7f
Ashish Kumar <ashish@192.168.1.4>
2023-07-27 17:55:58 +0530

    added articles page

--- app.js
+++ app.js
@@ -8,15 +8,16 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 const path = require('path')
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'public')))
 
-app.get("/" , function(req , res)
-{
+app.get("/" , function(req , res){
     res.render("home");
-})
+});
 
-app.listen(3000, function() 
-{
+app.get('/article' , (req,res) => {
+    res.render('article');
+});
+
+app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
 
