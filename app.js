de974139e721eec333b2528013840698b8270eab
Nilendra Ranawat <nilendraranawat@gmail.com>
2023-08-02 16:02:28 +0530

    Connected database and updated cards to fetch from database

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Require
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -9,8 +10,35 @@ app.use(bodyParser.urlencoded({extended: true}));
 const path = require('path')
 app.use(express.static(path.join(__dirname, 'public')))
 
+// MongoDB
+mongoose.set("strictQuery", false);
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+const postSchema = new mongoose.Schema({
+    title : {type : String , require : true},
+    content : {type : String , require : true},
+    tags : []
+});
+
+const Post = new mongoose.model("Post" , postSchema);
+
 app.get("/" , function(req , res){
-    res.render("home");
+    Post.find({} , (err , foundPost) => {
+        if(!err){
+            res.render("home" , {foundPost : foundPost});
+        } else{
+            console.log(err);
+        }
+    })
 });
 
 app.get('/article' , (req,res) => {
@@ -25,7 +53,28 @@ app.get('/register' , (req,res) => {
     res.render('register');
 });
 
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+app.post("/addpost" , (req , res) => {
+    const newPost = new Post({
+        title : req.body.title,
+        content : req.body.content,
+        tags : ["Kewis hamilton" , "kakaka"]
+    })
+    newPost.save((err) => {
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            console.log("Saved");
+        }
+    });
+});
+
+connectDB().then(() => {
+    console.log("DRC DB CONNECTED SUCCESFULLY");
+    app.listen(process.env.PORT || 3000, () => {
+        console.log("DRC SERVER STARTED");
+    });
 });
 
