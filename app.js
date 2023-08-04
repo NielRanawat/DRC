8b5a375bf2d1ca4600050c88248b0717d502377a
Nilendra Ranawat <nilendraranawat@gmail.com>
2023-08-04 15:58:54 +0530

    Login/Signup done

--- app.js
+++ app.js
@@ -5,12 +5,27 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
 const mongoose = require("mongoose");
-const lodash = require("lodash");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 const path = require('path');
-const { url } = require("inspector");
 app.use(express.static(path.join(__dirname, 'public')))
+
+// Initializing session and passport
+app.use(session({
+    secret : process.env.PASSPORT_KEY,
+    resave : false,
+    saveUninitialized : false,
+    cookie : {
+        expires : 600000
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // MongoDB
 mongoose.set("strictQuery", false);
@@ -25,6 +40,21 @@ const connectDB = async () => {
     }
 };
 
+const userSchema = new mongoose.Schema({
+    username : {type : String , require : true},
+    email : {type : String , require : true},
+    name : {type : String , require : true},
+    isAdmin : {type : Boolean , require : true , default : false},
+    password : String
+});
+
+userSchema.plugin(passportLocalMongoose);
+const User = new mongoose.model("User" , userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 const postSchema = new mongoose.Schema({
     title : {type : String , require : true},
     content : {type : String , require : true},
@@ -33,10 +63,52 @@ const postSchema = new mongoose.Schema({
 
 const Post = new mongoose.model("Post" , postSchema);
 
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+app.post("/register", function (req, res) {
+    User.register({username: req.body.username,  email: req.body.username, name: req.body.name} , req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            // res.render("alerts/uaxerror");
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/");
+            });
+        }
+    });
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/badcred"
+}), function (req, res) {
+});
+
+app.get('/login' , (req,res) => {
+    res.render('login');
+});
+
+app.get("/logout" , (req , res) => {
+    req.logout(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/login")
+        }
+    });
+})
+
 app.get("/" , function(req , res){
+    
     Post.find({} , (err , foundPost) => {
         if(!err){
-            res.render("home" , {foundPost : foundPost});
+            if(req.isAuthenticated()){
+                res.render("home" , {foundPost : foundPost , loggedIn : true});
+            }else{
+                res.render("home" , {foundPost : foundPost , loggedIn : false});
+            }
         } else{
             console.log(err);
         }
@@ -46,37 +118,50 @@ app.get("/" , function(req , res){
 app.get('/get-arcticles/:article_id' , (req,res) => {
     Post.findOne({_id : req.params.article_id} , function(err , foundPost){
         if (!err) {
-            res.render("article" , {foundPost : foundPost});
+            if(req.isAuthenticated()){
+                res.render("article" , {foundPost : foundPost , loggedIn : true});
+            }else{
+                res.render("article" , {foundPost : foundPost , loggedIn : false});
+            }
         }
     });
 });
 
-app.get('/login' , (req,res) => {
-    res.render('login');
-});
-
-app.get('/register' , (req,res) => {
-    res.render('register');
-});
 
 app.get('/admin' , (req,res) => {
-    res.render('admin');
+    if(req.isAuthenticated()){
+        if(req.user.isAdmin){
+            res.render('admin');
+        } else{
+            res.redirect("/");
+        }
+    } else{
+        res.redirect("/login");
+    }
 });
 
 app.post("/addpost" , (req , res) => {
-    const newPost = new Post({
-        title : req.body.title,
-        content : req.body.content,
-        // img : req.body.image
-    })
-    newPost.save((err) => {
-        if(err){
-            console.log(err);
+    if(req.isAuthenticated()){
+        if(req.user.isAdmin){
+            const newPost = new Post({
+                title : req.body.title,
+                content : req.body.content,
+                // img : req.body.image
+            });
+            newPost.save((err) => {
+                if(err){
+                    console.log(err);
+                } else{
+                    console.log("Saved");
+                    res.redirect("/");
+                }
+            });
         } else{
-            console.log("Saved");
             res.redirect("/");
         }
-    });
+    } else{
+        res.redirect("/login");
+    }
 });
 
 connectDB().then(() => {
