5ef4407dd9605c0dd05ec466bc791b26e7c981d1
Ashish Kumar <ashish@192.168.0.104>
2024-02-05 16:11:51 +0530

    v2 api route changes

--- app.js
+++ app.js
@@ -11,17 +11,18 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 const path = require('path');
+const { type } = require("os");
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Initializing session and passport
 app.use(session({
-    secret : process.env.PASSPORT_KEY,
-    resave : false,
-    saveUninitialized : false,
-    cookie : {
-        expires : 600000
+    secret: process.env.PASSPORT_KEY,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
     },
     store: new MongoStore({ mongooseConnection: mongoose.connection })
 }));
@@ -43,42 +44,44 @@ const connectDB = async () => {
 };
 
 const userSchema = new mongoose.Schema({
-    username : {type : String , require : true},
-    email : {type : String , require : true},
-    name : {type : String , require : true},
-    isAdmin : {type : Boolean , require : true , default : false},
-    password : String
+    username: { type: String, require: true },
+    email: { type: String, require: true },
+    name: { type: String, require: true },
+    isAdmin: { type: Boolean, require: true, default: false },
+    password: String
 });
 
 userSchema.plugin(passportLocalMongoose);
-const User = new mongoose.model("User" , userSchema);
+const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 const postSchema = new mongoose.Schema({
-    title : {type : String , require : true},
-    content : {type : String , require : true},
-    img_url : {type : String , require : true},
-    author_name : {type : String , require : true},
-    carouselHeading : {type : String , require : true , default : null},
-    carousel_id : {type : Number , require : true , default : null},
-    tags : [],
-    approved : {type : Boolean , require : true , default : false}
-},{
+    title: { type: String, require: true },
+    content: { type: String, require: true },
+    img_url: { type: String, require: true },
+    author_name: { type: String, require: true },
+    carouselHeading: { type: String, require: true, default: null },
+    carousel_id: { type: Number, require: true, default: null },
+    mainTag: { type: String, require: true, enum: ['f1', 'motogp', 'imsp'] },
+    tags: [],
+    status: { type: String, require: true, default: 'Pending', enum: ['Pending', 'Approved', 'Rejected'] },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+}, {
     timestamps: true
 });
 
 
-const Post = new mongoose.model("Post" , postSchema);
+const Post = new mongoose.model("Post", postSchema);
 
 app.get("/register", function (req, res) {
     res.render("register");
 });
 
 app.post("/register", function (req, res) {
-    User.register({username: req.body.username,  email: req.body.username, name: req.body.name} , req.body.password, function (err, user) {
+    User.register({ username: req.body.username, email: req.body.username, name: req.body.name }, req.body.password, function (err, user) {
         if (err) {
             console.log(err);
             // res.render("alerts/uaxerror");
@@ -96,28 +99,28 @@ app.post("/login", passport.authenticate("local", {
 }), function (req, res) {
 });
 
-app.get('/login' , (req,res) => {
-    if(req.isAuthenticated()){
+app.get('/login', (req, res) => {
+    if (req.isAuthenticated()) {
         res.redirect("/profile");
     } else {
         res.render('login');
     }
 });
 
-app.get("/logout" , (req , res) => {
-    req.logout(function(err){
-        if(err){
+app.get("/logout", (req, res) => {
+    req.logout(function (err) {
+        if (err) {
             console.log(err);
-        }else{
+        } else {
             res.redirect("/login")
         }
     });
 })
 
-app.get("/" , function(req , res){
-    Post.find({approved : true}).sort({ createdAt: -1 }).exec((err, foundPost) => {
-        if(!err){
-            if(req.isAuthenticated()){
+app.get("/", function (req, res) {
+    Post.find({ status: 'Approved' }).sort({ createdAt: -1 }).exec((err, foundPost) => {
+        if (!err) {
+            if (req.isAuthenticated()) {
                 let carousel1 = 0;
                 let carousel1found = false;
                 let carousel2 = 0;
@@ -125,21 +128,21 @@ app.get("/" , function(req , res){
                 let carousel3 = 0;
                 let carousel3found = false;
                 foundPost.forEach((post) => {
-                    if(post.carousel_id == 1 && !carousel1found){
+                    if (post.carousel_id == 1 && !carousel1found) {
                         carousel1 = post;
                         carousel1found = true;
                     }
-                    if(post.carousel_id == 2 && !carousel2found){
+                    if (post.carousel_id == 2 && !carousel2found) {
                         carousel2 = post;
                         carousel2found = true;
                     }
-                    if(post.carousel_id == 3 && !carousel3found){
+                    if (post.carousel_id == 3 && !carousel3found) {
                         carousel3 = post;
                         carousel3found = true;
                     }
                 });
-                res.render("home" , {foundPost : foundPost , loggedIn : true ,user : req.user.name, carousel1 : carousel1 , carousel2 : carousel2 , carousel3 : carousel3});
-            } else{
+                res.render("home", { foundPost: foundPost, loggedIn: true, user: req.user.name, carousel1: carousel1, carousel2: carousel2, carousel3: carousel3 });
+            } else {
                 let carousel1 = 0;
                 let carousel1found = false;
                 let carousel2 = 0;
@@ -147,99 +150,100 @@ app.get("/" , function(req , res){
                 let carousel3 = 0;
                 let carousel3found = false;
                 foundPost.forEach((post) => {
-                    if(post.carousel_id == 1 && !carousel1found){
+                    if (post.carousel_id == 1 && !carousel1found) {
                         carousel1 = post;
                         carousel1found = true;
                     }
-                    if(post.carousel_id == 2 && !carousel2found){
+                    if (post.carousel_id == 2 && !carousel2found) {
                         carousel2 = post;
                         carousel2found = true;
                     }
-                    if(post.carousel_id == 3 && !carousel3found){
+                    if (post.carousel_id == 3 && !carousel3found) {
                         carousel3 = post;
                         carousel3found = true;
                     }
                 });
-                res.render("home" , {foundPost : foundPost , loggedIn : false , user : null , carousel1 : carousel1 , carousel2 : carousel2 , carousel3 : carousel3});
+                res.render("home", { foundPost: foundPost, loggedIn: false, user: null, carousel1: carousel1, carousel2: carousel2, carousel3: carousel3 });
             }
-        } else{
+        } else {
             console.log(err);
         }
     });
 });
 
-app.get('/article/:article_id' , (req,res) => {
+app.get('/article/:article_id', (req, res) => {
     console.log(req.params.article_id);
-    Post.findOne({_id : req.params.article_id} , function(err , foundPost){
+    Post.findOne({ _id: req.params.article_id }, function (err, foundPost) {
         // const content = foundPost.content;
         if (!err) {
-            if(req.isAuthenticated()){
-                res.render("article" , {foundPost : foundPost , loggedIn : true , user : req.user.name});
-            }else{
-                res.render("article" , {foundPost : foundPost , loggedIn : false , user : null});
+            if (req.isAuthenticated()) {
+                res.render("article", { foundPost: foundPost, loggedIn: true, user: req.user.name });
+            } else {
+                res.render("article", { foundPost: foundPost, loggedIn: false, user: null });
             }
         }
     });
 });
 
 
-app.get("/admin" , (req,res)=>{
-    if(req.isAuthenticated()){
-        if(req.user.isAdmin){
+app.get("/admin", (req, res) => {
+    if (req.isAuthenticated()) {
+        if (req.user.isAdmin) {
             res.render('admin');
-        } else{
+        } else {
             res.redirect('/')
         }
-    } else{
+    } else {
         res.redirect("/login");
     }
 });
 
-app.get('/profile' , async (req,res) => {
-    if(req.isAuthenticated()){
-        res.render('profile' , {user : req.user.name , admin : req.user.isAdmin});
+app.get('/profile', async (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render('profile', { user: req.user.name, admin: req.user.isAdmin });
     } else {
         res.redirect('/login');
     }
 });
 
-app.get('/add-post' , (req,res) => {
-    if(req.isAuthenticated()){
-        res.render('add-post');
-    } else{
+app.get('/add-post', (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render('add-post' , {user : req.user.name});
+    } else {
         res.redirect("/login");
     }
 });
 
-app.get("/delete-post" , (req,res) => {
-    if(req.isAuthenticated()){
-        if(req.user.isAdmin){
-            Post.find().sort({ createdAt: -1 }).exec((err, foundPost) => {
+app.get("/articles-list", (req, res) => {
+    if (req.isAuthenticated()) {
+        if (req.user.isAdmin) {
+            Post.find({status : 'Approved'}).sort({ createdAt: -1 }).exec((err, foundPost) => {
                 if (err) {
-                  console.error(err);  
+                    console.error(err);
                 } else {
-                    res.render('delete-post' , {foundPost : foundPost});
-                }                
+                    res.render('articles-list', { foundPost: foundPost });
+                }
             });
-        } else{
+        } else {
             res.status(404).render("404");
         }
-    } else{
+    } else {
         res.redirect("/login");
-    } 
+    }
 });
 
-app.post("/delete-post" , (req,res) => {
-    if(req.isAuthenticated()){
-        if(req.user.isAdmin){
-            Post.findOneAndDelete({_id : req.body.article_id} , (err)=>{
+app.post("/delete-post", (req, res) => {
+    if (req.isAuthenticated()) {
+        if (req.user.isAdmin) {
+            console.log(req.body);
+            Post.findOneAndDelete({ _id: req.body.article_id }, (err) => {
                 if (!err) {
-                    res.redirect("/admin")
+                    res.redirect("/articles-list?returnMsg=postDeleted")
                 } else {
                     console.log(err);
                 }
             })
-        } else{
+        } else {
             res.redirect('/');
         }
     } else {
@@ -247,12 +251,12 @@ app.post("/delete-post" , (req,res) => {
     }
 });
 
-app.get('/delete-article-confirmation/:article_id' , async (req,res) => {
-    if(req.isAuthenticated()){
-        if(req.user.isAdmin){
-            const foundArticle = await Post.findOne({_id : req.params.article_id , approved : true});
+app.get('/delete-article-confirmation/:article_id', async (req, res) => {
+    if (req.isAuthenticated()) {
+        if (req.user.isAdmin) {
+            const foundArticle = await Post.findOne({ _id: req.params.article_id, status: 'Approved' });
             if (foundArticle != null) {
-                res.render('delete-posts' , {post : foundArticle});
+                res.render('delete-posts', { post: foundArticle });
             } else {
                 res.render('404');
             }
@@ -264,13 +268,13 @@ app.get('/delete-article-confirmation/:article_id' , async (req,res) => {
     }
 });
 
-app.get('/view-pending-article?' , async (req,res) => {
-    if(req.isAuthenticated){
-        if(req.user.isAdmin){
+app.get('/view-pending-article?', async (req, res) => {
+    if (req.isAuthenticated) {
+        if (req.user.isAdmin) {
             try {
-                const foundArticle = await Post.findOne({_id : req.query.article_id, approved : false});
+                const foundArticle = await Post.findOne({ _id: req.query.article_id, status: 'Pending' });
                 if (foundArticle != null) {
-                    res.render("unparticle" , {post : foundArticle})
+                    res.render("unparticle", { post: foundArticle , source : 'admin' })
                 } else {
                     res.render('404');
                 }
@@ -285,37 +289,39 @@ app.get('/view-pending-article?' , async (req,res) => {
     }
 });
 
-app.post("/add-post" , (req , res) => {
-    if(req.isAuthenticated()){
-            const newPost = new Post({
-                title : req.body.title,
-                content : req.body.content,
-                img_url : req.body.img_url,
-                author_name : req.body.author_name,
-            });
-            newPost.save((err) => {
-                if(err){
-                    console.log(err);
-                } else{
-                    res.redirect("/")
-                }
-            }); 
-    } else{
+app.post("/add-post", (req, res) => {
+    if (req.isAuthenticated()) {
+        const newPost = new Post({
+            title: req.body.title,
+            content: req.body.content,
+            img_url: req.body.img_url,
+            author_name: req.body.author_name,
+            mainTag: 'f1',
+            createdBy: req.user._id
+        });
+        newPost.save((err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.redirect("/user-articles?returnMsg=postAdded")
+            }
+        });
+    } else {
         res.redirect("/login");
     }
 });
 
-app.get("/pending-articles" , (req , res) => {
-    if(req.isAuthenticated()){
-        if(req.user.isAdmin){
-            Post.find({approved : false} , (err , foundPost)=>{
+app.get("/pending-articles", (req, res) => {
+    if (req.isAuthenticated()) {
+        if (req.user.isAdmin) {
+            Post.find({ status: 'Pending' }, (err, foundPost) => {
                 if (!err) {
-                    res.render("pending-article" , {foundPost : foundPost});
+                    res.render("pending-article", { foundPost: foundPost });
                 } else {
                     console.log(err);
                 }
-            })
-        } else{
+            });
+        } else {
             res.status(404).render("404");
         }
     } else {
@@ -323,13 +329,13 @@ app.get("/pending-articles" , (req , res) => {
     }
 });
 
-app.post("/approve-post" , (req , res) => {
-    if(req.isAuthenticated()){
-        if(req.user.isAdmin){
-            Post.findOne({_id : req.body.id} , (err , foundPost) => {
-                res.render("edit-post" , {foundPost : foundPost});
+app.post("/approve-post", (req, res) => {
+    if (req.isAuthenticated()) {
+        if (req.user.isAdmin) {
+            Post.findOne({ _id: req.body.id }, (err, foundPost) => {
+                res.render("edit-post", { foundPost: foundPost });
             })
-        } else{
+        } else {
             res.status(404).render("404");
         }
     } else {
@@ -338,29 +344,59 @@ app.post("/approve-post" , (req , res) => {
 });
 
 
-app.get('/category?' , async (req,res) => {
-    const foundArticles = await Post.find({approved : true ,  tags : req.query.name}).sort({ createdAt: -1 });
-    if(req.isAuthenticated()){
-        res.render("category" , {foundPost : foundArticles , loggedIn : true , user : req.user.name , category : req.query.name});
+app.get('/category?', async (req, res) => {
+    const foundArticles = await Post.find({ status: 'Approved', tags: req.query.name }).sort({ createdAt: -1 });
+    if (req.isAuthenticated()) {
+        res.render("category", { foundPost: foundArticles, loggedIn: true, user: req.user.name, category: req.query.name });
     } else {
-        res.render("category" , {foundPost : foundArticles , loggedIn : false , user : null , category : req.query.name});
+        res.render("category", { foundPost: foundArticles, loggedIn: false, user: null, category: req.query.name });
     }
 });
 
 
-app.post("/edit-post" , (req , res) => {
-    if(req.isAuthenticated()){
-        if(req.user.isAdmin){
+app.get('/user-articles', async (req, res) => {
+    if (req.isAuthenticated()) {
+        try {
+            const foundPosts = await Post.find({ createdBy: req.user._id }).populate('createdBy').exec();
+            res.render('user-articles', { foundPosts: foundPosts , user : req.user.name})
+        } catch (error) {
+            console.log(error);
+        }
+    } else {
+        res.redirect('/login')
+    }
+});
+
+app.get('/user-unpublished-article/:article_id', async (req, res) => {
+    if (req.isAuthenticated()) {
+        try {
+            const foundPost = await Post.findOne({ _id : req.params.article_id , createdBy : req.user._id  });
+            if(foundPost != null){
+                res.render('unparticle', { post : foundPost ,source : 'user'});
+            } else {
+                res.render('404');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    } else {
+        res.redirect('/login')
+    }
+});
+
+app.post("/edit-post", (req, res) => {
+    if (req.isAuthenticated()) {
+        if (req.user.isAdmin) {
             const gotTagString = req.body.tags;
             const tagArray = gotTagString.split(',');
-            Post.findOneAndUpdate({_id : req.body.id} , {title : req.body.title , content : req.body.content, img_url : req.body.img_url , author_name : req.body.author_name ,carousel_id : req.body.carousel_id , carouselHeading : req.body.carouselHeading , tags : tagArray , approved : true} , (err) => {
-                if(err){
+            Post.findOneAndUpdate({ _id: req.body.id }, { title: req.body.title, content: req.body.content, img_url: req.body.img_url, author_name: req.body.author_name, carousel_id: req.body.carousel_id, carouselHeading: req.body.carouselHeading, tags: tagArray, status: 'Approved' }, (err) => {
+                if (err) {
                     console.log(err);
-                } else{
-                    res.redirect("/pending-articles");
+                } else {
+                    res.redirect("/articles-list?returnMsg=postPublished");
                 }
             });
-        } else{
+        } else {
             res.redirect('/');
         }
     } else {
@@ -368,13 +404,13 @@ app.post("/edit-post" , (req , res) => {
     }
 });
 
-app.get('/edit-post/:article_id' , async (req,res) => {
-    if(req.isAuthenticated()){
-        if(req.user.isAdmin){
+app.get('/edit-post/:article_id', async (req, res) => {
+    if (req.isAuthenticated()) {
+        if (req.user.isAdmin) {
             try {
-                const foundPost = await Post.findOne({_id : req.params.article_id , approved : true});
-                if(foundPost != null) {
-                    res.render("edit-post" , {foundPost : foundPost});
+                const foundPost = await Post.findOne({ _id: req.params.article_id, status: 'Approved' });
+                if (foundPost != null) {
+                    res.render("edit-post", { foundPost: foundPost });
                 } else {
                     res.render('404');
                 }
@@ -384,7 +420,7 @@ app.get('/edit-post/:article_id' , async (req,res) => {
         } else {
             res.redirect('/');
         }
-    } else { 
+    } else {
         res.redirect('/login');
     }
 });
